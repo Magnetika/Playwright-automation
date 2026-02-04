304701b9b14e8028f401718d5c990b12d2d251b2
Magnetika <agneslilla.szabo@gmail.com>
2026-02-04 16:09:27 +0100

    upload file spec add

---
+++ tests/upload.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect} from '@playwright/test';
+const path = require('path');
+
+test.describe('Upload file', () => {
+
+    test('should upload a test file', async ({ page }) => {
+
+        //Open URL
+
+        await page.goto('https://practice.sdetunicorns.com/cart/');
+
+        //provide test file path
+
+        const filePath = path.join(__dirname, '../data/pikachu.png');
+
+        //upload test file
+
+        await page.setInputFiles('input#upfile_1', filePath);
+
+        //click the submit button
+
+        await page.locator('#upload_1').click();
+
+        //assertion
+
+        await expect(page.locator('#wfu_messageblock_header_1_label_1'))
+            .toContainText('uploaded successfully');
+
+    })
+})
