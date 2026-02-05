99c2cf36176f283e53f5d39ce8aed34bd7eae104
Magnetika <agneslilla.szabo@gmail.com>
2026-02-05 18:11:23 +0100

    add import, class, constructor to upload component

--- pages/component/upload.component.ts
+++ pages/component/upload.component.ts
@@ -1,10 +1,17 @@
-import {Page} from '@playwright/test';
+import {Page, Locator} from '@playwright/test';
 
 class UploadComponent {
     page: Page;
+    uploadInput: string;
+    submitButton: Locator;
+    successText: Locator;
+
 
     constructor(page: Page) {
         this.page = page;
+        this.uploadInput = 'input#upfile_1';
+        this.submitButton = page.locator('#upload_1');
+        this.successText = page.locator('#wfu_messageblock_header_1_label_1');
     }
 }
 
